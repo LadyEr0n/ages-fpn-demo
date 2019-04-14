ee3342d912753523779992d6b157e94933df99d5
LadyEr0n <erin.gruenwedel@loop.colum.edu>
2019-04-14 17:31:18 -0500

    Fixed sound issue in menu

--- Assets/Scripts/InventoryMenu.cs
+++ Assets/Scripts/InventoryMenu.cs
@@ -1,12 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
 
 public class InventoryMenu : MonoBehaviour
 {
     private static InventoryMenu instance;
     private CanvasGroup canvasGroup;
-
+    private RigidbodyFirstPersonController rigidbodyFirstPersonController;
+    private AudioSource audioSource;
     public static InventoryMenu Instance
     {
         get
@@ -20,16 +22,29 @@ public class InventoryMenu : MonoBehaviour
 
     private bool IsVisible => canvasGroup.alpha > 0;
 
+    public void ExitMenuButtonClicked()
+    {
+        HideMenu();
+    }
+
     private void ShowMenu()
     {
         canvasGroup.alpha = 1;
         canvasGroup.interactable = true;
+        rigidbodyFirstPersonController.enabled = false;
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+        audioSource.Play();
     }
 
     private void HideMenu()
     {
         canvasGroup.alpha = 0;
         canvasGroup.interactable = false;
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+        rigidbodyFirstPersonController.enabled = true;
+        audioSource.Play();
     }
 
     private void Update()
@@ -54,6 +69,24 @@ public class InventoryMenu : MonoBehaviour
             throw new System.Exception("There is already an instance of InventoryMenu and there can only be one.");
 
         canvasGroup = GetComponent<CanvasGroup>();
+        rigidbodyFirstPersonController = FindObjectOfType<RigidbodyFirstPersonController>();
+        audioSource = GetComponent<AudioSource>();
+    }
+    private void Start()
+    {
         HideMenu();
+        StartCoroutine(WaitForAudioClip());
+        Debug.Log("We're not done waiting.");
+    }
+
+    private IEnumerator WaitForAudioClip()
+    {
+        float originalVolume = audioSource.volume;
+        audioSource.volume = 0;
+        Debug.Log("Start waiting.");
+        yield return new WaitForSeconds(audioSource.clip.length);
+        Debug.Log("Done Waiting.");
+        audioSource.volume = originalVolume;
+
     }
 }
