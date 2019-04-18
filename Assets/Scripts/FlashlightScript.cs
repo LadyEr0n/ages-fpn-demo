94fbfc84ead2d0fd1b4e48311674e7b739bda32f
LadyEr0n <erin.gruenwedel@loop.colum.edu>
2019-04-18 17:53:12 -0500

    Added flashlight script

---
+++ Assets/Scripts/FlashlightScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlashlightScript : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject playerLight;
+
+    [SerializeField]
+    private bool isOn;
+
+    private new Light light;
+    // Start is called before the first frame update
+
+    private void Start()
+    {
+        light = playerLight.GetComponent<Light>();
+        isOn = false;
+    }
+
+    private void Update()
+    {
+        if(Input.GetButtonDown("Flashlight"))
+        {
+            isOn = !isOn;
+        }
+        if (isOn == true)
+        {
+            light.enabled = true;
+        }
+        else
+        {
+            light.enabled = false;
+        }
+    }
+}
